23418fa75cc7c6045038d3befc3b4869e126b1ac
root <huy.nguyenquang@alta.com.vn>
2019-04-06 12:49:40 +0700

    Code html change pass

--- src/app/user/change_pass/change_pass.component.ts
+++ src/app/user/change_pass/change_pass.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangePassService } from './change_pass.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-change-pass',
   templateUrl: './change_pass.component.html',
-  styleUrls: ['./change_pass.component.css']
+  styleUrls: ['./change_pass.component.css'],
+  providers: [ ChangePassService ]
 })
 export class ChangePassComponent implements OnInit {
+  private old_pass:string = '';
+  private new_pass:string = '';
+  private retype_new_pass:string = '';
 
-  constructor() { }
+  constructor(private change_pass_service:ChangePassService) { }
 
   ngOnInit() {
+     
+
   }
+
+  handleChangePass() {
+    if(this.new_pass !== this.retype_new_pass && this.retype_new_pass !== "")
+      Swal.fire('Error', 'New password and retype is not valid', 'error');
+
+    if(this.old_pass === this.new_pass && this.new_pass !== '')
+      Swal.fire('Error', 'New password must be different from old password', 'error');
+  }
+
 
 }
