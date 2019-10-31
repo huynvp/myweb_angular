a210db8968bf229a475fbbdf52c0d0ac3389549b
System Administrator <root@Macs-MacBook-Pro.local>
2019-10-31 23:51:59 +0700

    Fix

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,11 +3,12 @@ import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import Swal from 'sweetalert2';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['../login/login.component.css']
 })
 export class SignupComponent implements OnInit {
   public name:'';
@@ -18,7 +19,7 @@ export class SignupComponent implements OnInit {
   public password:'';
   public re_password:'';
 
-  constructor(public router:Router, public http:HttpClient) {
+  constructor(public router:Router, public http:HttpClient, private spinner: NgxSpinnerService) {
     if(localStorage.token !== undefined) {
 			this.router.navigate(['/home']);
 		}
@@ -28,6 +29,7 @@ export class SignupComponent implements OnInit {
   }
 
   handleSignup() {
+    this.spinner.show();
     let json = JSON.stringify({
       name: this.name,
       email: this.email,
@@ -42,9 +44,11 @@ export class SignupComponent implements OnInit {
     })
     .toPromise()
     .then(data => {
+      this.spinner.hide();
       Swal.fire('Success', 'Signup success, please login go to system', 'success');
     })
     .catch(err => {
+      this.spinner.hide();
       console.log(err);
       Swal.fire('Error', err['error']['message'], 'error');
     })
