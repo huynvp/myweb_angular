33c1ab5dce7886e0832544086976a997a785a090
Huy Nguyen <huy.nguyenquang@alta.com.vn>
2020-03-25 23:01:27 +0700

    Update

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -3,6 +3,7 @@ import { Routes,RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IndexComponent } from "./index.component";
+import { NzModalModule, NzListModule, NzSkeletonModule } from 'ng-zorro-antd';
 
 const routes: Routes = [
     {
@@ -16,6 +17,9 @@ const routes: Routes = [
         RouterModule.forChild(routes),
         CommonModule,
         FormsModule,
+        NzModalModule,
+        NzListModule,
+        NzSkeletonModule
     ],
     declarations: [
         IndexComponent
